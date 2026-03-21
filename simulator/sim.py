f14e688169c84aed6aabd194063ea37e5d0b50cf
Gabe-lacey <gvlacey@uwm.edu>
2026-03-21 17:05:28 -0500

    created simulation skeliton

---
+++ simulator/sim.py
@@ -0,0 +1,19 @@
+class transistor:
+    def __init__(self):
+        self.base = None
+        self.emitter = None
+        self.collector = None
+class wire:
+    def __init__(self):
+        self.input = None
+        self.output = None
+class input:
+    def __init__(self):
+        self.output = None
+class delay:
+    def __init__(self):
+        self.input = None
+class junction:
+    def __inti__(self):
+        self.input = None
+        self.output = []

4b4ef27eb1990507cf19396fc7c748b20fbc30db
Gabe-lacey <gvlacey@uwm.edu>
2026-03-21 19:54:19 -0500

    changed plans, added 4 basic logic ics

--- simulator/sim.py
+++ simulator/sim.py
@@ -1,36 +1,52 @@
-class transistor:
+class pin:
     def __init__(self):
-        self.base = None
-        self.emitter = None
-        self.collector = None
-        self.Vbe = 0
-        self.saturationVoltage = 0
-        self.outputVoltage = 0
+        self.output = False
+class IC7408:
+    def __init__(self):
+        self.pins = [pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin]
     def update(self):
-        Vb = self.base.outputVoltage
-        Vc = self.collector.outputVoltage
-        if Vb >= self.saturationVoltage:
-            self.outputVoltage = Vc
-        elif self.base < self.Vbe:
-            self.outputVoltage = 0
+        if(self.pins[6].output == False and self.pins[13] == 1):
+            self.pins[2] = self.pins[1] and self.pins[0]
+            self.pins[5] = self.pins[4] and self.pins[3]
+            self.pins[7] = self.pins[8] and self.pins[9]
+            self.pins[10] = self.pins[11] and self.pins[12]
         else:
-            self.outputVoltage = Vb-self.Vbe
+            self.pins[2] = self.pins[5] = self.pins[7] = self.pins[10] = False
+class IC7432:
+    def __init__(self):
+        self.pins = [pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin]
+    def update(self):
+        if(self.pins[6].output == False and self.pins[13] == 1):
+            self.pins[2] = self.pins[1] or self.pins[0]
+            self.pins[5] = self.pins[4] or self.pins[3]
+            self.pins[7] = self.pins[8] or self.pins[9]
+            self.pins[10] = self.pins[11] or self.pins[12]
+        else:
+            self.pins[2] = self.pins[5] = self.pins[7] = self.pins[10] = False
+class IC7486:
+    def __init__(self):
+        self.pins = [pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin]
+    def update(self):
+        if(self.pins[6].output == False and self.pins[13] == 1):
+            self.pins[2] = self.pins[1] == self.pins[0]
+            self.pins[5] = self.pins[4] == self.pins[3]
+            self.pins[7] = self.pins[8] == self.pins[9]
+            self.pins[10] = self.pins[11] == self.pins[12]
+        else:
+            self.pins[2] = self.pins[5] = self.pins[7] = self.pins[10] = False
+class IC7406:
+    def __init__(self):
+        self.pins = [pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin, pin]
+    def update(self):
+        if(self.pins[6].output == False and self.pins[13] == 1):
+            self.pins[1] = not self.pins[0]
+            self.pins[3] = not self.pins[2]
+            self.pins[5] = not self.pins[4]
+            self.pins[7] = not self.pins[8]
+            self.pins[9] = not self.pins[10]
+            self.pins[11] = not self.pins[12]
+        else:
+            self.pins[2] = self.pins[5] = self.pins[7] = self.pins[10] = False
 
-
-class input:
-    def __init__(self):
-        self.output = None
-        self.outputVoltage = 0
-class delay:
-    def __init__(self):
-        self.input = None
-        self.outputVoltage = 0
-class junction:
-    def __inti__(self):
-        self.input = None
-        self.output = []
-        self.outputVoltage = 0
-class circuit:
-    def __init__(self):
-        self.inputs = []
-        self.outputs = []
+class memoryIC:
+    pass
